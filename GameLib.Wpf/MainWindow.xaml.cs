6a95bfc946e98114b43c9651949fb238613c3730
modo-lv <modo@modo.lv>
2017-03-19 23:57:40 +0200

    Added WPF project.

---
+++ GameLib.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GameLib.Wpf.ViewModels;
+using Simpler.Net.Wpf;
+
+namespace GameLib.Wpf {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private readonly MainViewModel _viewModel;
+		private readonly SimplerWindow _win;
+
+		public MainWindow() {
+			this._win = new SimplerWindow(this);
+			this._win.FullyLoaded += this.WindowFullyLoaded;
+
+			this.InitializeComponent();
+
+			this._viewModel = new MainViewModel();
+
+			this.DataContext = this._viewModel;
+		}
+
+		private void WindowFullyLoaded(Object sender, EventArgs e)
+		{
+			this._viewModel.ActivityText = "Loading Steam games...";
+		}
+	}
+}
---
+++ GameLib.Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simpler.Net.Wpf;
+
+namespace GameLib.Wpf.ViewModels {
+	public class MainViewModel : Bindable {
+		private String _activityText;
+
+		public String ActivityText
+		{
+			get => this._activityText;
+			set => this.SetAndNotify(ref this._activityText, value);
+		}
+	}
+}
